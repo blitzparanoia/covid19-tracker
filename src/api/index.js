7adb618e32a5fba6ee6caf257d5f3aed6493c9d7
blitzparanoia <bluemilk44@gmail.com>
2020-04-26 20:01:30 -0400

    implemented use of react hooks for chart state and async changes with useState and useEffect

--- src/api/index.js
+++ src/api/index.js
@@ -19,3 +19,14 @@ export const fetchData = async () => {
 
     }
 }
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+
+        console.log(data)
+
+    } catch (error) {
+
+    }
+}
