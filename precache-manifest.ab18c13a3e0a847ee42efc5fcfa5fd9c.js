f4254a435376d5fd87c462d83e2ac266983d2cdb
blitzparanoia <bluemilk44@gmail.com>
2020-04-27 10:34:16 -0400

    Updates

---
+++ precache-manifest.ab18c13a3e0a847ee42efc5fcfa5fd9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071a2383bfb76dddc05cd3a8836b0802",
+    "url": "/covid19-tracker/index.html"
+  },
+  {
+    "revision": "d31bf6c5ec81a9d87cec",
+    "url": "/covid19-tracker/static/css/main.b6787383.chunk.css"
+  },
+  {
+    "revision": "d7115e26f91a43b793a9",
+    "url": "/covid19-tracker/static/js/2.99e64d9e.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19-tracker/static/js/2.99e64d9e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d31bf6c5ec81a9d87cec",
+    "url": "/covid19-tracker/static/js/main.4300325e.chunk.js"
+  },
+  {
+    "revision": "fac2a894e78d6ae19981",
+    "url": "/covid19-tracker/static/js/runtime-main.e1b4b4ae.js"
+  }
+]);
